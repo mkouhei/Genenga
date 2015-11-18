a82b1e8d15c48a80731e02bef7212e747a741c71
Kouhei Maeda <mkouhei@palmtb.net>
2015-11-19 05:06:11 +0900

    Refactors; modifies module's docstring.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- genenga/tests/test_control.py
+++ genenga/tests/test_control.py
@@ -1,21 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-    <one line to give the program's name and a brief idea of what it does.>
-    Copyright (C) 2012-2014 Kouhei Maeda <mkouhei@palmtb.net>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
+"""genenga.tests.test_control."""
 import os
 import unittest
 from genenga import control as c
